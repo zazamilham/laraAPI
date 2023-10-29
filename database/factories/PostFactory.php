6954b6407661f00e6fc43534f814f64144584b8a
Mohammad Reza Ebrahimipoor <aghaye.ebi@gmail.com>
2023-10-29 22:18:48 +0330

    Create Lavarel app and create user and post model

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     * @throws \Exception
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->title,
+            'slug' => fake()->slug,
+            'image'=> fake()->image,
+            'content'=>fake()->text,
+            'user_id' => random_int(1,10)
+        ];
+    }
+}
