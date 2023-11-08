6c191b9ceb8f36e9dd5857c98bbc479a72cbe852
Mohammad Reza Ebrahimipoor <aghaye.ebi@gmail.com>
2023-11-08 20:02:30 +0330

    [implement] exception handler

--- app/APiService/ApiResponse.php
+++ app/APiService/ApiResponse.php
@@ -7,18 +7,14 @@ use Illuminate\Http\JsonResponse;
 trait ApiResponse
 {
 
-    public function success($data = null, $code = 200, $message = 'Successfully'):JsonResponse
+    public function success($data = null, $code = 200, $message = 'Successfully'): JsonResponse
     {
-        try {
-            return response()->json(['data' => $data , 'message' => $message], $code);
-        }catch (\Exception $e) {
-            $this->error($e->getMessage());
-        }
+        return response()->json(['data' => $data, 'message' => $message], $code);
     }
 
-    public function error($data = null, $code = 404, $message = 'Error'):JsonResponse
+    public function error($data = null, $code = 404, $message = 'Error'): JsonResponse
     {
-        return response()->json(['data' => $data , 'message' => $message], $code);
+        return response()->json(['data' => $data, 'message' => $message], $code);
     }
 
 }
--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,16 @@
 
 namespace App\Exceptions;
 
+use App\APiService\ApiResponse;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use phpDocumentor\Reflection\Types\Parent_;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponse;
+
     /**
      * The list of the inputs that are never flashed to the session on validation exceptions.
      *
@@ -26,5 +31,15 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if ($e instanceof ModelNotFoundException) {
+            return $this->error('', 404, $e->getMessage());
+        }
+        if (config('app.debug')) {
+            return Parent::render($request, $e);
+        }
     }
 }
